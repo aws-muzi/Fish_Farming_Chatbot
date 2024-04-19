b49af3ae918ff6e9066a1cfea1809e5154ef0be3
aws-muzi <151033572+aws-muzi@users.noreply.github.com>
2024-04-19 06:39:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -37,11 +37,39 @@ class MultiApp:
                 styles={
                     "container": {"padding": "5!important"},  #61AEA6
                     "icon": {"color": "#1a725f", "font-size": "20px"}, 
-                    "nav-link": {"color":"#1a725f","font-size": "20px", "text-align": "left", "margin":"0px", "--hover-color": "#90e0ef"},  #a1e3dc
+                    "nav-link": {"color":"#1a725f","font-size": "20px", "text-align": "left", "margin":"0px", "--hover-color": "gray"},  #a1e3dc
                     "nav-link-selected": {"background-color": "#1a725f","color":"white"},  #1A7269
                     }
         
                 )
+            notepad = st.checkbox(":orange[write notes]")
+            if notepad:
+                # Text area for user to enter notes
+                notes = st.text_area("Take your notes here:")
+
+                # Text input for filename
+                filename = st.text_input("Enter a filename for your notes:", "your_notes.txt")
+
+                # Function to generate a text file from notes
+                def get_text_file(notes):
+                    # Convert the notes to bytes
+                    return notes.encode('utf-8')
+
+                # Display the save button and download button conditionally
+                if st.button("Save Notes"):
+                    # Call the function to get text file
+                    tmp_file = get_text_file(notes)
+                    # Check if filename has .txt extension, add if not
+                    if not filename.endswith('.txt'):
+                        filename += '.txt'
+                    # Create download button for the text file
+                    st.download_button(
+                        label="Download Notes",
+                        data=tmp_file,
+                        file_name=filename,
+                        mime="text/plain",
+                        help="Click to download your notes as a TXT file."
+                    )
             
             
         if app == "Forum":
